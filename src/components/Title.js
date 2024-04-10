13e912c052456b68cc66d329aae0e97eda7ffc74
meganclapinski25 <142531799+meganclapinski25@users.noreply.github.com>
2024-04-10 10:38:39 -0700

    started responsive

--- src/components/Title.js
+++ src/components/Title.js
@@ -9,21 +9,22 @@ import RandomSpace from './RandomSpace/RandomSpace'
 function Title() {
   return (
     <div className="Title">
-      <header>
-        <h1>SFPOPOS</h1>
-        <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
-
-        <div>
-        <NavLink 
-	        className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
-	        to="/">List</NavLink>
-        <NavLink 
-	        className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
-	        to="/about">About</NavLink>
-          <RandomSpace />
+        <div className='titles'>
+          <h1>SFPOPOS</h1>
+          <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+        </div>
+        <div className='items' >
+          <NavLink 
+            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+            to="/">List</NavLink>
+          <NavLink 
+            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+            to="/about">About</NavLink>
+            <RandomSpace /> 
+            {/* when button is active make it responsive */}
         </div>
 
-      </header>
+      
     </div>
   )
 }
